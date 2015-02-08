6e33e5021167bf3f277cf44d946f308a3c9fccf6
yasha0b1 <jacob.govshteyn@gmail.com>
2015-02-08 17:28:57 -0600

    fixed idx

--- get.bat
+++ get.bat
@@ -3,7 +3,7 @@ set /a idx=1
 setlocal ENABLEDELAYEDEXPANSION
 for /F "tokens=*" %%A in  ( %1) do  (
   CALL rtmpdump  -r rtmp://flash.dce.harvard.edu/bounce -C B:0 -C Z: -C S:/%%A -C S:BounceAPI3.0 -C N:0.000000 -C S:mp4 -y mp4:%%A -o video!idx!.mp4
-   set /a idx=idx + 1
+   set /a idx=idx + 1 
 )
 endlocal
 PAUSE
