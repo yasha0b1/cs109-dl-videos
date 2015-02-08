fe533a527d164e8411857800e4366e2b09336d89
yasha0b1 <jacob.govshteyn@gmail.com>
2015-02-08 12:02:22 -0600

    windows version of get script

---
+++ get.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+set idx=1
+for /F "tokens=*" %%A in  ( %1) do  (
+   ECHO %%A %idx%
+   CALL rtmpdump -r rtmp://flash.dce.harvard.edu/bounce -C B:0 -C Z: -C S:/%%A -C S:BounceAPI3.0 -C N:0.000000 -C S:mp4 -y mp4:%%A -o video%idx%.mp4  
+)
+PAUSE
+CLS
